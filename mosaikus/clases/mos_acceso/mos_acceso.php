15cab03e0941f9023db84f1bcd5fc2144488040f
Deris <Deris@192.168.5.111>
2016-04-10 11:29:16 -0430

    JD, Version 1.6.2 para control de menu obtener nodos de la estructura marcados y su estructura completa

--- mosaikus/clases/mos_acceso/mos_acceso.php
+++ mosaikus/clases/mos_acceso/mos_acceso.php
@@ -35,7 +35,25 @@
             else 
                 return $arbol;
         } 
-        
+
+        public function recursiveNodosArbol($nodoArbol){
+            $atr = array();
+            $this->nodoArbol = $nodoArbol;
+            $sql = 'SELECT o.* FROM mos_organizacion o WHERE o.id ='.$this->nodoArbol['parent_id'];            
+            $this->operacion($sql, $atr);
+            $repo = $this->dbl->data;
+            
+            global $arbol;            
+            array_push($arbol,$repo[0]);
+            
+            $this->nodoArbol['parent_id'] = $repo[0]['parent_id'];            
+            if($repo[0]['parent_id'] > 1)
+                $this->recursiveNodosArbol($this->nodoArbol);
+            else 
+                return $arbol;
+            
+            
+        }
         public function obtenerHijosMenu($coleccion, $padre){
             $param=array();
             foreach ($coleccion as $arrC){               
@@ -50,8 +68,107 @@
             
             return $param;
         }
-
         
+        public function obtenerNodosArbolNivel($coleccion, $padre){
+            $param=array();
+            foreach ($coleccion as $arrC){               
+                if($arrC['parent_id'] == $padre && $arrC['title'] != '')
+                    array_push($param, $arrC);
+            }  
+            
+            foreach ($param as $clave => $fila) {
+                $orden[$clave] = $fila['position'];                    
+            }
+            array_multisort($orden, SORT_ASC, $param); 
+            
+            return $param;
+        }
+        
+        public function obtenerArbolEstructura($usuario, $modulo, $modo){
+            $atr = array();
+            if($modo == 'Especialista')
+                $sql = "SELECT
+                        mos_o.*
+                FROM
+                        mos_usuario_estructura AS mos_ue 
+                        INNER JOIN mos_perfil AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                        INNER JOIN mos_link_por_perfil AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                        INNER JOIN mos_link AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                        INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                WHERE 
+                        mos_ue.portal = 'N'
+                        AND mos_ue.id_usuario =$usuario
+                        AND mos_l.cod_link = $modulo
+                    ";
+            else
+                $sql = "SELECT
+                        mos_o.*
+                FROM
+                    mos_usuario_estructura AS mos_ue 
+                    INNER JOIN mos_perfil_portal AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                    INNER JOIN mos_link_por_perfil_portal AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                    INNER JOIN mos_link_portal AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                    INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                WHERE 
+                        mos_ue.portal = 'S'
+                        AND mos_ue.id_usuario =$usuario
+                        AND mos_l.cod_link = $modulo
+                    ";
+            $this->operacion($sql, $atr);
+            global $arbol;
+            $arbol = $this->dbl->data;
+            if($modo == 'Especialista')
+                $sql = "SELECT
+                        distinct mos_o.parent_id
+                FROM
+                        mos_usuario_estructura AS mos_ue 
+                        INNER JOIN mos_perfil AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                        INNER JOIN mos_link_por_perfil AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                        INNER JOIN mos_link AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                        INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                WHERE 
+                        mos_ue.portal = 'N'
+                        AND mos_ue.id_usuario =$usuario
+                        AND mos_l.cod_link = $modulo
+                    ";
+            else
+                $sql = "SELECT
+                        distinct mos_o.parent_id
+                FROM
+                    mos_usuario_estructura AS mos_ue 
+                    INNER JOIN mos_perfil_portal AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                    INNER JOIN mos_link_por_perfil_portal AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                    INNER JOIN mos_link_portal AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                    INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                WHERE 
+                        mos_ue.portal = 'S'
+                        AND mos_ue.id_usuario =$usuario
+                        AND mos_l.cod_link = $modulo
+                    ";
+                
+            $this->operacion($sql, $atr);
+            $padres = $this->dbl->data;  
+            foreach($padres as $arrN){
+                $this->recursiveNodosArbol($arrN);
+            }
+
+            //ordenar el arbol obtenido
+            asort($arbol);
+            
+
+            //recorro 
+            foreach ($arbol as $k1 => $v1) {
+                foreach ($arbol as $k2 => $v2) {
+                    if ($k1 != $k2) {
+                        if ($v1 == $v2) {
+                            unset($arbol[$k1]);
+                        }
+                    }
+                }
+            }
+            
+            return $arbol;
+        }
         public function obtenerNodosArbol($usuario, $modulo, $modo){
             $atr = array();
             if($modo == 'Especialista')
@@ -66,6 +183,7 @@
                         ,mos_p.recordatorio
                         ,mos_p.modificar_terceros
                         ,mos_p.visualizar_terceros
+                        ,mos_o.parent_id
                 FROM
                         mos_usuario_estructura AS mos_ue 
                         INNER JOIN mos_perfil AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
@@ -89,6 +207,7 @@
                         ,mos_p.recordatorio
                         ,mos_p.modificar_terceros
                         ,mos_p.visualizar_terceros
+                        ,mos_o.parent_id
                 FROM
                     mos_usuario_estructura AS mos_ue 
                     INNER JOIN mos_perfil_portal AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
@@ -105,6 +224,70 @@
             $arbol = $this->dbl->data;
             return $arbol;                        
         }
+
+
+/**
+         * Devuelve el array con los permisos del usuario sobre un modulo
+         * @param int $usuario id del usuario
+         * @param int $modulo id del modulo
+         * @param int $id_area id del area
+         * @return array
+         */
+        public function obtenerPermisosModulo($usuario, $modulo,$id_area = null){
+            $permisos = array();
+            if ($id_area == NULL){
+                $sql = "SELECT
+                            mos_l.cod_link
+                            ,mos_l.nombre_link
+                            -- ,mos_o.id
+                            -- ,mos_o.title
+                            ,mos_p.nuevo
+                            ,mos_p.modificar
+                            ,mos_p.eliminar
+                            ,mos_p.recordatorio
+                            ,mos_p.modificar_terceros
+                            ,mos_p.visualizar_terceros
+                    FROM
+                            mos_usuario_filial AS mos_ue 
+                            INNER JOIN mos_perfil AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                            INNER JOIN mos_link_por_perfil AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                            INNER JOIN mos_link AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                            -- INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                    WHERE 
+                            -- mos_ue.portal = 'N'
+                           -- AND
+                            mos_ue.id_usuario =$usuario
+                            AND mos_l.cod_link = $modulo";
+            }
+            else{
+                $sql = "SELECT
+                            mos_l.cod_link
+                            ,mos_l.nombre_link
+                            -- ,mos_o.id
+                            -- ,mos_o.title
+                            ,mos_p.nuevo
+                            ,mos_p.modificar
+                            ,mos_p.eliminar
+                            ,mos_p.recordatorio
+                            ,mos_p.modificar_terceros
+                            ,mos_p.visualizar_terceros
+                    FROM
+                            mos_usuario_estructura AS mos_ue 
+                            INNER JOIN mos_perfil AS mos_p ON mos_ue.cod_perfil = mos_p.cod_perfil
+                            INNER JOIN mos_link_por_perfil AS mos_lp ON mos_ue.cod_perfil = mos_lp.cod_perfil
+                            INNER JOIN mos_link AS mos_l On mos_lp.cod_link = mos_l.cod_link
+                            -- INNER JOIN mos_organizacion AS mos_o ON mos_ue.id_estructura = mos_o.id
+                    WHERE 
+                            mos_ue.id_estructura = $id_area
+                           -- AND
+                            and mos_ue.id_usuario =$usuario
+                            AND mos_l.cod_link = $modulo";
+            }
+            //echo $sql;
+            $permisos = $this->dbl->query($sql);
+            return $permisos;
+        }
+        
         public function obtenerNodosMenu($usuario, $filial, $modo){
             $atr = array();
             if($modo == 'Especialista')
