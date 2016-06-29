c64c9b5c06da63b2272664725a02fd9a0bc27bae
Andres Zambrano <Andres Zambrano@192.168.0.162>
2016-06-29 18:18:49 -0400

    Se ejecuto el scaffold para generar el modelo DocumentosMigracion, donde se hicieron modificaciones en: se creo el documentomigracion.js

---
+++ mosaikus/dist/js/documentos_migracion/documentos_migracion.js
@@ -0,0 +1,124 @@
+
+    
+    function init_filtrar(){        
+            PanelOperator.initPanels('');
+            ScrollBar.initScroll();
+            init_filtro_rapido();
+    }
+
+    function filtrar_mostrar_colums(){
+        var colums = '1-2-3-4-';
+         $('.checkbox-mos-col').each(function() {
+                if (this.checked){
+                    colums = colums + this.value + '-';
+                }
+         });
+         colums = colums.substring(0, colums.length - 1);
+         $('#mostrar-col').val(colums);
+         verPagina($('#pag_actual').val(),1);
+         $('#myModal-Mostrar-Colums').modal('hide');
+         
+    }
+
+    function nuevo_DocumentosMigracion(){
+            array = new XArray();
+            array.setObjeto('DocumentosMigracion','crear');
+            array.addParametro('modo',document.getElementById('modo').value);            
+            array.addParametro('cod_link',document.getElementById('cod_link').value);            
+            array.addParametro('import','clases.documentos_migracion.DocumentosMigracion');
+            xajax_Loading(array.getArray());
+    }
+
+    function validar(doc){
+        var ejecutar = function (){
+            if($('#idFormulario').isValid()) {
+                $( "#btn-guardar" ).html('Procesando..');
+                $( "#btn-guardar" ).prop( "disabled", true );
+                array = new XArray();
+                if (doc.getElementById("opc").value == "new")
+                    array.setObjeto('DocumentosMigracion','guardar');
+                else
+                    array.setObjeto('DocumentosMigracion','actualizar');
+                array.addParametro('permiso',document.getElementById('permiso_modulo').value);
+                array.getForm('idFormulario');
+                array.addParametro('modo',document.getElementById('modo').value);            
+                array.addParametro('cod_link',document.getElementById('cod_link').value);            
+                array.addParametro('import','clases.documentos_migracion.DocumentosMigracion');
+                xajax_Loading(array.getArray());
+            }        
+            else{
+        
+             }
+        }
+        bootbox.confirm("Al realizar esta operación, se va a migrar las responsabilidades del usuario"
+                       + $("#id_responsable_actual option:selected").text() + ","
+                       + "esto no se podrá deshacer. ¿Desea continuar?", function(result) {
+                    if (result == true){
+                        ejecutar();
+                    }
+
+                }); 
+
+    }
+
+    function editarDocumentosMigracion(id){
+        array = new XArray();
+        array.setObjeto('DocumentosMigracion','editar');
+        array.addParametro('id',id);
+        array.addParametro('import','clases.documentos_migracion.DocumentosMigracion');
+        xajax_Loading(array.getArray());
+    }
+
+
+    function eliminarDocumentosMigracion(id){
+        if(confirm("¿Desea Eliminar el DocumentosMigracion Seleccionado?")){
+            array = new XArray();
+            array.setObjeto('DocumentosMigracion','eliminar');
+            array.addParametro('id',id);
+            array.addParametro('permiso',document.getElementById('permiso_modulo').value);
+            array.addParametro('import','clases.documentos_migracion.DocumentosMigracion');
+            xajax_Loading(array.getArray());
+        }
+    }
+    function verPagina(pag,doc){
+        array = new XArray();
+        if (doc== null)
+        {
+             $('form')[0].reset();             
+        }
+        array.getForm('busquedaFrm'); 
+        if ((isNaN(document.getElementById("reg_por_pag").value) == true) || (parseInt(document.getElementById("reg_por_pag").value) <= 0)){
+            array.addParametro('reg_por_pagina', 10);
+            document.getElementById("reg_por_pag").value = 10
+        }
+        else
+        {
+            array.addParametro('reg_por_pagina', document.getElementById("reg_por_pag").value);
+        }
+        array.addParametro('permiso',document.getElementById('permiso_modulo').value);
+        array.addParametro('pag',pag);
+        array.setObjeto('DocumentosMigracion','buscar');
+        array.addParametro('import','clases.documentos_migracion.DocumentosMigracion');
+        $('#MustraCargando').show();
+        xajax_Loading(array.getArray());
+    }
+
+    function verDocumentosMigracion(id){
+        var src = 'pages/' +  document.getElementById("modulo_actual").value + '/verDocumentosMigracion.php?id='+id;
+        $('a#ver_ficha_trabajador').fancybox({
+                    'titleShow': false,
+                    'href' : src,
+                    'autoDimensions' :true,
+                    'type':'iframe'                    
+                });        
+        $("#ver_ficha_trabajador").trigger('click');        
+    }
+    function ActivaPersonal(valor, obj){
+       if (valor == 'N'){
+           $('#'+obj).val('').change();
+           $('#'+obj).prop('disabled', true);
+       }
+       else{
+           $('#'+obj).prop('disabled', false);
+       }
+    }    
