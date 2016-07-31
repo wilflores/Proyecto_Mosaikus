461ed3f0d71e443d66ef2d652a5bca041b1b7bad
Andres Zambrano <Andres Zambrano@192.168.0.162>
2016-07-31 19:19:19 -0430

    se creo la tabla mos_nombres_link_idiomas , mos_idioma Se agrego un campo nuevo en mos_usuario con el id_idioma Incluye un query para duplicar mos_nombres_campos de cocumentos, registros y usuarios

--- mosaikus/andres.sql
+++ mosaikus/andres.sql
@@ -422,3 +422,228 @@ INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES (
 INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'num_rev', 'Revisión', '6', 'Revisión');
 INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'arbol_organizacional', 'Árbol Organizacional (Niveles)', '6', 'Árbol Organizacional (Niveles)');
 INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'fecha_rev', 'Fecha de Revisión', '6', 'Fecha de Revisión');
+
+/****************************************/
+/*cambio del 27-07*/
+/****************************************/
+
+ALTER TABLE `mos_usuario`
+ADD COLUMN `id_idioma`  int NULL DEFAULT 1 AFTER `recibe_notificaciones`;
+
+DROP TABLE IF EXISTS `mos_idiomas`;
+CREATE TABLE `mos_idiomas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idioma` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of mos_idiomas
+-- ----------------------------
+INSERT INTO `mos_idiomas` VALUES ('1', 'Español');
+INSERT INTO `mos_idiomas` VALUES ('2', 'Portugues');
+
+INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'password_2', 'Nuevo Password', '21', 'Nuevo Password');
+INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'password_3', 'Confirme Password ', '21', 'Confirme Password');
+INSERT INTO `mos_nombres_campos`(nombre_campo,texto,modulo,placeholder) VALUES ( 'id_idioma', 'Idioma', '21', 'Idioma');
+
+-- CON ESTO DUPLICAMOS LOS REGISTROS DE DOCUMENTOS, MOS_USUARIO Y REGISTROS 
+insert into mos_nombres_campos (nombre_campo, texto, modulo, placeholder,id_idioma)
+SELECT nombre_campo, texto, modulo, placeholder, 2 idioma
+FROM `mos_nombres_campos`
+WHERE modulo in (21,6,9) ORDER BY modulo
+
+
+DROP TABLE IF EXISTS `mos_nombres_link_idiomas`;
+CREATE TABLE `mos_nombres_link_idiomas` (
+  `cod_link` int(11) NOT NULL DEFAULT '0',
+  `id_idioma` int(11) NOT NULL DEFAULT '0',
+  `nombre_link` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cod_link`,`id_idioma`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of mos_nombres_link_idiomas
+-- ----------------------------
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('1', '1', 'Indicadores');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('1', '2', 'indicadores');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('2', '1', 'Matriz');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('2', '2', 'matriz');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('3', '1', 'Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('3', '2', 'documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('4', '1', 'Estructura');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('4', '2', 'estrutura');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('5', '1', 'Personal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('5', '2', 'pessoal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('6', '1', 'Accidentes Ley');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('6', '2', 'acidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('7', '1', 'Incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('7', '2', 'incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('8', '1', 'Acciones Correctivas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('8', '2', 'ocorrências');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('9', '1', 'Competencias');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('9', '2', 'competências');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('10', '1', 'Configuración');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('10', '2', 'configuração');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('11', '1', 'Administrador de Matriz Aspectos - Impactos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('11', '2', 'gestor de matriz - aspectos e impactos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('12', '1', 'Administrador de Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('12', '2', 'gestor de documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('13', '1', 'Reportes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('13', '2', 'resumos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('14', '1', 'Maestro de Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('14', '2', 'mestre de documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('15', '1', 'Maestro de Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('15', '2', 'mestre de documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('16', '1', 'Maestro de Registros');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('16', '2', 'mestre de registros');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('17', '1', 'Reporte documentos por Árbol');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('17', '2', 'resumo de documentos por árvore');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('18', '1', 'Árbol Organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('18', '2', 'árvore organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('19', '1', 'Árbol de Procesos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('19', '2', 'árvore de processos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('20', '1', 'Personal Directo');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('20', '2', 'pessoal direto');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('21', '1', 'Listado de Cargos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('21', '2', 'lista de cargos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('22', '1', 'Administrador de actividades de Capacitación');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('22', '2', 'gestor da atividades de treinamento');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('23', '1', 'Reporte Personal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('23', '2', 'resumo de pessoal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('24', '1', 'Reporte Personal PDF');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('24', '2', 'resumo pessoal pdf');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('25', '1', 'Fichas de cursos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('25', '2', 'ficha de cursos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('26', '1', 'Reporte Capacitaciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('26', '2', 'resumo de capacitação');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('27', '1', 'Reporte Personal XLS');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('27', '2', 'resumo de pessoal xls');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('28', '1', 'Administrador de Accidentes Ley');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('28', '2', 'gerente lei de acidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('29', '1', 'Cierres de Mes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('29', '2', 'fechamento de mês');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('30', '1', 'reportes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('30', '2', 'resumos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('31', '1', 'Registro Accidente Ley');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('31', '2', 'registro de acidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('32', '1', 'Cierre Mes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('32', '2', 'fechamento de mês');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('33', '1', 'Tasas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('33', '2', 'taxas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('34', '1', 'Estadística Distributiva');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('34', '2', 'estatísticas distributivas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('35', '1', 'Estadística Semestral');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('35', '2', 'estatísticas semestrais');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('36', '1', 'Condiciones Lógicas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('36', '2', 'condições lógicas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('37', '1', 'Aplicación DS 67');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('37', '2', 'aplicação nr xx');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('38', '1', 'Estadistica Mensual');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('38', '2', 'estatísticas mensais');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('39', '1', 'Estadistica Mensual Informe');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('39', '2', 'relatório estatístico mensal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('40', '1', 'Cierre de Mes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('40', '2', 'fechamento de mês');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('41', '1', 'Cierre de Mes Por Árbol de Procesos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('41', '2', 'fechamento de mês por árvore de processos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('42', '1', 'Cierre de Mes Por Árbol Organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('42', '2', 'fechamento de mês por árvore organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('43', '1', 'Tasas Web');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('43', '2', 'taxas de web');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('44', '1', 'Tasas Excel');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('44', '2', 'taxas de excel');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('45', '1', 'Tasas Gráficos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('45', '2', 'taxas gráficos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('46', '1', 'Maestro Accidentes PDF');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('46', '2', 'mestre de acidentes pdf');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('47', '1', 'Maestro Accidentes Excel');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('47', '2', 'mestre de acidentes xls');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('48', '1', 'Informes Individuales');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('48', '2', 'relatórios individuais');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('49', '1', 'Árbol Organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('49', '2', 'árvore organizacional');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('50', '1', 'Administrador de  AC');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('50', '2', 'gestor de ocorrências com plano de acção');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('51', '1', 'Configuración de AC');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('51', '2', 'configuração');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('52', '1', 'Reportes de AC');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('52', '2', 'resumos ocorrências com plano de acção');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('55', '1', 'Incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('55', '2', 'incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('56', '1', 'reportes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('56', '2', 'resumos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('57', '1', 'Incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('57', '2', 'incidentes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('58', '1', 'Competencia familia');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('58', '2', 'família de competências');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('59', '1', 'Competencias requisitos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('59', '2', 'requisitos de competências');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('60', '1', 'Competencias cargos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('60', '2', 'competências cargos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('61', '1', 'Competencias personas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('61', '2', 'competências de pessoas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('62', '1', 'reportes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('62', '2', 'resumos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('63', '1', 'Reporte Competencias - Matriz');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('63', '2', 'resumo competências de pessoas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('64', '1', 'Usuarios');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('64', '2', 'usuários');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('65', '1', 'Parámetros Módulos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('65', '2', 'parâmetros modules');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('66', '1', 'Administrador de Perfiles');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('66', '2', 'gestor de perfil');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('67', '1', 'Unidad de Negocio');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('67', '2', 'unidade de negócio');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('68', '1', 'Configuración');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('68', '2', 'configuração');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('69', '1', 'Parámetro Formulario');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('69', '2', 'formulário de parâmetro');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('70', '1', 'Parámetros Generales Sistema');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('70', '2', 'parâmetros gerales  do sistema');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('71', '1', 'Reportes');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('71', '2', 'resumos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('72', '1', 'Excel');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('72', '2', 'excel');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('73', '1', 'Capacitaciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('73', '2', 'capacitações');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('74', '1', 'Personal Indirecto');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('74', '2', 'pessoal indireto');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('75', '1', 'Parámetros de Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('75', '2', 'configurações de documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('76', '1', 'Parámetros de Personas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('76', '2', 'parâmetros de pessoas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('77', '1', 'Administrar Correcciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('77', '2', 'gestor de ocorrências com correções');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('78', '1', 'Parámetros de Correcciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('78', '2', 'parâmetros correções');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('79', '1', 'Parámetros de AC');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('79', '2', 'parâmetros ac');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('80', '1', 'Perfiles Especialistas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('80', '2', 'perfis de especialistas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('81', '1', 'Perfiles Portal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('81', '2', 'perfis Portal');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('82', '1', 'WorkFlow de Documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('82', '2', 'WorkFlow de documentos');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('83', '1', 'Inspecciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('83', '2', 'inspeções');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('84', '1', 'Plantillas de Inspecciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('84', '2', 'modelos de inspecções');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('85', '1', 'Notificaciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('85', '2', 'notificações');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('86', '1', 'Parametros de Inspecciones');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('86', '2', 'parâmetros de inspeção');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('87', '1', 'Administrador de Formularios');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('87', '2', 'Forms Manager');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('88', '1', 'Parámetros de Formularios');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('88', '2', 'parâmetros de formulários');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('89', '1', 'Configuración');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('89', '2', 'configuração');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('90', '1', 'Códigos de Areas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('90', '2', 'códigos de Áreas');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('91', '1', 'Lista de Distribución');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('91', '2', 'Lista de distribuição');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('92', '1', 'Migración de Responsabilidades');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('92', '2', 'migração de Responsabilidades');
+INSERT INTO `mos_nombres_link_idiomas` VALUES ('93', '2', 'Seguimento de Ação Corretiva');
+
